8b8c4d66345392dca87bf1b703e78800fbd64537
can doğanci <canndoganci@hotmail.com>
2021-04-04 20:52:48 +0300

    04.04.2021

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -8,11 +8,17 @@ import { ListResponseModel } from '../models/listResponseModel';
   providedIn: 'root'
 })
 export class CarService {
-  apiUrl = "https://localhost:44312/api/cars/getall";
+  apiUrl = "https://localhost:44312/api/";
 
   constructor(private httpClient:HttpClient) { }
 
   getCars():Observable<ListResponseModel<Car>>{
-   return this.httpClient.get<ListResponseModel<Car>>(this.apiUrl);
+    let newPath = this.apiUrl+"cars/getall"
+   return this.httpClient.get<ListResponseModel<Car>>(newPath);
   }
+
+  getCarsByColor(colorId:number):Observable<ListResponseModel<Car>>{
+    let newPath = this.apiUrl+"cars/getbycolor?colorId="+colorId
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+   }
 }
